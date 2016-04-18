8463705c944663c338437d703276386c382ba383
GILSON DA SILVA VIEIRA DE PAULA <31409695@mackenzie.br>
2016-04-18 10:39:57 -0300

    Implementing DAO

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -1,14 +1,30 @@
 package dao;
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-public interface Dao<E> {
+public abstract class Dao<E> {
 
-	public E findById(String id);
-	
-	public List<E> findAll();
-	
-	public void save(E e);
-	
-	public boolean delete(E e);
+    protected EntityManager entityManager;
+    
+    protected EntityManagerFactory entityManagerFactory;
+
+    public Dao() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("bookstore_pu");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+    
+    protected void openConnection() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+    
+    public abstract E findById(String id);
+
+    public abstract List<E> findAll();
+
+    public abstract void save(E e);
+
+    public abstract boolean delete(E e);
 }
