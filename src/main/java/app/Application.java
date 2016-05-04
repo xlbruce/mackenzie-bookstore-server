ec926653b98c640053104e4f4bd6630c9692e41a
Gilson Vieira <gilson.vieira@clickbus.com>
2016-05-04 17:52:40 -0300

    Added configuration package to ComponentScan

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("controller")
+@ComponentScan({"controller", "config"})
 public class Application {
 
 	public static void main(String[] args) {
