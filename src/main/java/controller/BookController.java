0aeb3f55837a34fff237a6b04fb0a82f61f1e512
Gilson Vieira <gilson.vieira@clickbus.com>
2016-05-04 17:57:59 -0300

    Added the following endpoints:
    - /books -> returns all books in database
    - /books/{isbn} -> returns an book instance given an valid ISBN
    - /books/name/{name} -> returns an books list according the given name

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,8 +1,38 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BookController {
+import model.entities.Book;
 
+import repository.BookRepository;
+import service.BookRepositoryService;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+	
+	@Autowired
+	private BookRepositoryService bookRepositoryService;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Book> index() {
+		return bookRepositoryService.findAll();
+	}
+	
+	@RequestMapping("/{isbn}")
+	public Book findByIsbn(@PathVariable("isbn") String isbn) {
+		return bookRepositoryService.findById(isbn);
+	}
+	
+	@RequestMapping("/name/{name}")
+	public List<Book> findByName(@PathVariable String name) {
+		return bookRepositoryService.findByName(name);
+	}
+	
 }
